73f1eb57ffd54f8a98c9ba00defad6520b0e2cf1
zhaqq <qiuxiaoyong@linghit.com>
2019-01-02 16:24:54 +0800

    init

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container"> 
-        <span class="text-light"><i class="fa fa-heart text-danger"></i>  兔死狗烹，鸟尽弓藏</span>
-        <span class="text-warning pull-right"><i class="fa fa-copyright"></i>  <small>万事靠己</small></span>
+        <span class="text-light"><i class="fa fa-heart text-danger"></i>  君子不器  </span>
+        <span class="text-warning pull-right"><i class="fa fa-copyright"></i>  <small>粤ICP备18022203号-1</small></span>
     </div>
 </footer>
